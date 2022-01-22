3b12e3f22857197e53f63209f7c4aeb25f548056
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-22 22:50:28 +0530

    Create print_pattern2.cpp

---
+++ c++/pattern/print_pattern2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void Draw(int n)
+{
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+  return 0;
+}
