c2031308b3e31ae8311659c5fa36ab5e36c19cac
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-26 17:24:26 +0530

    Create clear.cpp

---
+++ c++/string/string_class/stringfunctions/clear.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    // s.clear will clear a string contaent
+    string s="program";
+    cout<<s<<endl;
+    s.clear();
+    cout<<s<<endl;
+    return 0;
+}
