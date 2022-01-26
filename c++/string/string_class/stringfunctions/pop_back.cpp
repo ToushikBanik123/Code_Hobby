ee549f757a2744a63b8681d15b79c0dd1b94fd04
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-26 16:47:22 +0530

    Create pop_back.cpp

---
+++ c++/string/string_class/stringfunctions/pop_back.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s="Programing";
+    // it will remove a character from the end of the string just like Backspace
+    s.pop_back();
+    cout<<s;
+    
