bf934d46d3cbf119e7073a4a09ce096c859d836d
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-26 16:55:51 +0530

    Create insert.cpp

---
+++ c++/string/string_class/stringfunctions/insert.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    // s.insert will insert an string on a spacific Index
+    string s="program";
+    s.insert(3,"bb");
+    cout<<s;
+    return 0;
+}
