bd2acea7f8a4eda6e5b939bcaf1fbf91f32ba18f
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-30 22:13:43 +0530

    Create print_a_string_characters_sung_iterator.cpp

---
+++ c++/string/print_a_string_characters_sung_iterator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "welcome";
+    string::iterator it;
+    int count = 0;
+    for (it = str.begin(); it != str.end(); it++)
+    {
+        count++;
+    }
+
+    cout << count;
+
+    return 0;
+}
